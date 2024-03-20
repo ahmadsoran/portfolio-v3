318b057c72eeb0add45773fa984091b254a95be8
ahmadsorann <ahmadsorann@gmail.com>
2024-03-20 21:22:39 +0300

    r fetch

--- src/components/sections/skills.section.tsx
+++ src/components/sections/skills.section.tsx
@@ -1,19 +1,7 @@
-import { baseUrl } from '@/constant/const';
-import { PersonalData } from '@/@types';
 import Skills from '../UI/skills';
-const SkillsData = async function SkillsData<T>(): Promise<T> {
-  return (await (
-    await fetch(`${baseUrl}/assets/personal-data.json`, {
-      // SSG
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
-  ).json()) as T;
-};
+import SkillsData from '../../../public/assets/personal-data.json';
 export default async function SkillsSection() {
-  const data = await SkillsData<PersonalData>();
+  const data = SkillsData;
   const skills = data.skills;
   return <Skills data={skills} />;
 }
